a4b0169d56a2d9b81b8a1f7d2e659e57995b7b7e
Felix-Hofer <felix_hofer@gmx.de>
2025-05-25 21:03:52 +0200

    Making directories dynamic

--- src/cycling_game/animation.py
+++ src/cycling_game/animation.py
@@ -1,21 +1,24 @@
 import os
 from cycling_game.utils import load_image
+from pathlib import Path
 
+BASE_DIR = Path(__file__).resolve().parent
+ASSETS_DIR = BASE_DIR / "Resources"
+BASE_IMG_PATH = ASSETS_DIR / "Images"
 
 def get_animation_list(sprite):
     
-    player_frame_dir = f"/Users/felixhofer/Documents/GitHub/cycling-game/src/cycling_game/Resources/Images/{sprite}"
-    
+    player_frame_dir = BASE_IMG_PATH / f"{sprite}"
     animation_list = []
 
     for file in sorted(os.listdir(player_frame_dir)):
-        frame = load_image(f"{sprite}/" + file)
+        frame = load_image(player_frame_dir / file)
         animation_list.append(frame)
 
     return animation_list
 
 def explosion_animation_list():
-    explosion_dir = "/Users/felixhofer/Documents/GitHub/cycling-game/src/cycling_game/Resources/Images/Explosion/"
+    explosion_dir = BASE_IMG_PATH / "Explosion"
     explosion_list = []
     for file in sorted(os.listdir(explosion_dir)):
         frame = load_image("Explosion/" + file)
